7b47c04f60e39fbd1f2c17e380d7c0f73ab65714
Anthony Dourado <135552243+Code-With-Anthony@users.noreply.github.com>
2024-12-16 19:50:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TestingPatternMatching from './compoents/TestingPatternMatching';
+import ImageUploader from './compoents/ImageUploader';
+import BoundingPattern from './compoents/BoundingPattern';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TestingPatternMatching/> */}
+      {/* <ImageUploader/> */}
+      <BoundingPattern/>
+    </div>
+  );
+}
+
+export default App;
